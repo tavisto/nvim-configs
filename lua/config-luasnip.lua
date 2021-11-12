4c4a7308948e54fff74281429dc9e59c620f2062
Tavis Aitken <taitken@splunk.com>
2021-11-12 13:23:40 -0700

    Added more language servers and made snippets lazyload.

--- lua/config-luasnip.lua
+++ lua/config-luasnip.lua
@@ -1,4 +1,2 @@
 --
-require("luasnip/loaders/from_vscode").load({ include = {"markdown"}})
-require("luasnip/loaders/from_vscode").load({ include = { "python" } }) -- Load only python snippets
-require("luasnip/loaders/from_vscode").load({ include = { "go" } }) -- Load only python snippets
+require("luasnip/loaders/from_vscode").lazy_load()
--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -33,7 +33,18 @@ end
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-local servers = { 'pyright', 'gopls'}
+local servers = {
+  'bashls',
+  'cssls',
+  'dockerls',
+  'eslint',
+  'gopls',
+  'html',
+  'jsonls',
+  'pyright',
+  'terraformls',
+  'yamlls',
+}
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
